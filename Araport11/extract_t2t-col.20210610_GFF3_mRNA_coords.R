d770a54db6c55228d46bdbfe9a4016eb32e5cc5c
ajtock <andytock@gmail.com>
2021-07-23 17:17:05 +0100

    extract mRNA coords and define ranLocs

--- Araport11/extract_t2t-col.20210610_GFF3_mRNA_coords.R
+++ Araport11/extract_t2t-col.20210610_GFF3_mRNA_coords.R
@@ -1,40 +1,46 @@
-#!/applications/R/R-3.5.0/bin/Rscript
+#!/applications/R/R-4.0.0/bin/Rscript
 
-# Create table of representative genes from GFF3 file
+# Create table of representative genes
 # and generate random loci of the same number and width distribution
 # Write as GFF3 and BED files 
 
 # Usage:
-# ./extract_GFF3_mRNA_coords.R Araport11_GFF3_genes_transposons.201606.gff
+# ./extract_mRNA_coords.R 'Chr1,Chr2,Chr3,Chr4,Chr5'
+
+#chrName <- unlist(strsplit("Chr1,Chr2,Chr3,Chr4,Chr5",
+#                           split = ","))
 
 args <- commandArgs(trailingOnly = T)
-inFile <- args[1]
+chrName <- unlist(strsplit(args[1],
+                           split = ","))
 
+options(stringsAsFactors = F)
 library(GenomicRanges)
 library(rtracklayer)
 library(parallel)
 library(data.table)
 
 # Genomic definitions
-fai <- read.table("TAIR10_chr_all.fa.fai", header = F, stringsAsFactors = F)
-chrs <- fai[,1][1:5]
-chrLens <- fai[,2][1:5]
-centromeres <- c(15086045, 3607929, 13587786, 3956021, 11725024)
-# Pericentromeric regions are as defined in Supplemental Table S26
-# of Ziolkowski et al. (2017) Genes Dev. 31
-pericenStart <- c(11330001, 990001, 10200001, 990001, 8890001)
-pericenEnd <- c(18480000, 7540000, 16860000, 6850000, 15650000)
-genomeGR <- GRanges(seqnames = chrs,
+fai <- read.table("/home/ajt200/analysis/nanopore/t2t-col.20210610/t2t-col.20210610.fa.fai", header = F)
+chrs <- fai$V1[1:5]
+chrLens <- fai$V2[1:5]
+regionGR <- GRanges(seqnames = chrs,
                     ranges = IRanges(start = rep(1, length(chrs)),
                                      end = chrLens),
                     strand = "*")
+CENstart <- c(14841110,3823792,13597188,4203902,11784131)[which(fai$V1 %in% chrName)]
+CENend <- c(17559778,6045243,15733925,6977949,14551809)[which(fai$V1 %in% chrName)]
+CENGR <- GRanges(seqnames = chrs,
+                 ranges = IRanges(start = CENstart,
+                                  end = CENend),
+                 strand = "*")
 
-genes <- readGFF(inFile)
-genes <- genes[!(genes$seqid %in% c("ChrC", "ChrM")),]
-genes$seqid <- gsub("Chr", "", genes$seqid)
+# Load table of gene coordinates in t2t-col.20210610 (GFF3 derived from Liftoff tool)
+genes <- readGFF("t2t-col.20210610.genes.gff3")
+genes <- genes[genes$seqid %in% chrName,]
 mRNA <- genes[genes$type == "mRNA",]
 print(dim(mRNA))
-#[1] 52060    25
+#[1] 35234    20
 
 # Obtain frequency of occurrence of each gene parent ID
 n_occur <- data.frame(table(unlist(mRNA$Parent)))
@@ -69,25 +75,25 @@ mRNA_multi_list <- mclapply(seq_along(mRNA_multi[,1]), function(h) {
 mRNA_multi_dup <- rbindlist(mRNA_multi_list)
 mRNA_multi_rep <- unique(as.data.frame(mRNA_multi_dup))
 
-
 # Combine into one representative set of mRNA entries, order,
 # and output in GFF3 and BED formats
 mRNA_rep <- rbind(mRNA_unique, mRNA_multi_rep)
-stopifnot(dim(mRNA_rep)[1] == dim(n_occur)[1])
 mRNA_rep <- mRNA_rep[ order(mRNA_rep$seqid,
                             mRNA_rep$start,
                             mRNA_rep$end), ]
 write.table(mRNA_rep[,1:9],
-            file = "Araport11_representative_mRNA.gff3",
+            file = paste0("t2t-col.20210610_representative_mRNA_",
+                          paste0(chrName, collapse = "_"), ".gff3"),
             quote = F, sep = "\t", row.names = F, col.names = F)
 mRNA_rep_bed <- data.frame(chr = as.character(mRNA_rep[,1]),
                            start = as.integer(mRNA_rep[,4]-1),
                            end = as.integer(mRNA_rep[,5]),
-                           name = as.integer(1:length(mRNA_rep[,1])),
+                           name = as.character(mRNA_rep[,9]),
                            score = as.numeric(mRNA_rep[,6]),
                            strand = as.character(mRNA_rep[,7]))
 write.table(mRNA_rep_bed,
-            file = "Araport11_representative_mRNA.bed",
+            file = paste0("t2t-col.20210610_representative_mRNA_",
+                          paste0(chrName, collapse = "_"), ".bed"),
             quote = F, sep = "\t", row.names = F, col.names = F)
 
 mRNA_repGR <- GRanges(seqnames = mRNA_rep$seqid,
@@ -111,13 +117,18 @@ set.seed(93750174)
 
 # Apply ranLocStartSelect() on a per-chromosome basis so that
 # ranLocGR contains the same number of loci per chromosome as mRNA_repGR
+chrs <- chrs[chrs %in% chrName]
 ranLocGR <- GRanges()
 for(i in 1:length(chrs)) {
   mRNA_repChrGR <- mRNA_repGR[seqnames(mRNA_repGR) == chrs[i]]
-  genomeChrGR <- genomeGR[seqnames(genomeGR) == chrs[i]]
-  # Contract genomeChrGR so that random loci end coordinates do not extend beyond chromosome ends
-  end(genomeChrGR) <- end(genomeChrGR)-max(width(mRNA_repChrGR))
-  ranLocChrStart <- ranLocStartSelect(coordinates = (start(genomeChrGR):end(genomeChrGR)),
+  regionChrGR <- regionGR[seqnames(regionGR) == chrs[i]]
+  # Contract regionChrGR so that random loci and 2-kb flanking regions
+  # do not extend beyond chromosome ends
+  end(regionChrGR) <- end(regionChrGR)-max(width(mRNA_repChrGR))-2000
+  start(regionChrGR) <- start(regionChrGR)+2000
+  ranLocChrStart <- ranLocStartSelect(coordinates = unlist(lapply(seq_along(regionChrGR), function(x) {           
+                                                             start(regionChrGR[x]) : end(regionChrGR[x])          
+                                                           })),
                                       n = length(mRNA_repChrGR))
   ranLocChrGR <- GRanges(seqnames = chrs[i],
                          ranges = IRanges(start = ranLocChrStart,
@@ -125,6 +136,7 @@ for(i in 1:length(chrs)) {
                          strand = strand(mRNA_repChrGR))
   ranLocGR <- append(ranLocGR, ranLocChrGR)
 }
+
 ranLoc_bed <- data.frame(chr = as.character(seqnames(ranLocGR)),
                          start = as.integer(start(ranLocGR)-1),
                          end = as.integer(end(ranLocGR)),
@@ -132,5 +144,6 @@ ranLoc_bed <- data.frame(chr = as.character(seqnames(ranLocGR)),
                          score = rep("NA", length(ranLocGR)),
                          strand = as.character(strand(ranLocGR)))
 write.table(ranLoc_bed,
-            file = "Araport11_representative_mRNA_randomLoci.bed",
+            file = paste0("t2t-col.20210610_representative_mRNA_",
+                          paste0(chrName, collapse = "_"), "_randomLoci.bed"),
             quote = F, sep = "\t", row.names = F, col.names = F)
